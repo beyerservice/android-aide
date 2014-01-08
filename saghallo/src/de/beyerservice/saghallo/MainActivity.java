3ac26db9632a0641f4e657480bfc30c2c5285944
beyerservice <beyerservice@gmail.com>
2014-01-08 23:15:08 +0100

    sag hallo mit tts import richtig

---
+++ saghallo/src/de/beyerservice/saghallo/MainActivity.java
@@ -0,0 +1,35 @@
+package de.beyerservice.saghallo;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+import android.speech.tts.*;
+/** das obere tut nicht. Er findet OnInitListener nicht  */
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import java.util.*;
+
+public class MainActivity extends Activity implements OnInitListener
+{
+
+	private TextToSpeech tts;
+
+    /** Called when the activity is first created. */
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+	{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+		tts = new TextToSpeech(this, this);
+    }
+
+	@Override
+	public void onInit(int arg0)
+	{
+		tts.speak("Hallo!", TextToSpeech.QUEUE_FLUSH, null);
+
+	}
+
+
+}
